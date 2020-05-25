e11dd7b0b714c19069754bbca1a187885aac50ee
ManShum <43279996+ManShum815@users.noreply.github.com>
2020-05-25 14:32:43 -0400

    Add files via upload

--- live/scan.sh
+++ live/scan.sh
@@ -1,1 +1,7 @@
-
+while read target; do
+if dig "$target" > /dev/null; then
+# The subdomain is outdated, then do not
+# put into a file called "target.txt".
+echo "$target" >> target.txt
+fi
+done < /root/Desktop/op.txt
