60ebbc470532fd2e0bf43a4635bda026adba0464
ManShum <43279996+ManShum815@users.noreply.github.com>
2020-05-25 13:19:28 -0700

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,47 @@
 
 package main
 
-while read target; do
-if dig "$target" > /dev/null; then
-# The subdomain is outdated, then do not
-# put into a file called "target.txt".
-echo "$target" >> target.txt
-fi
-done < /path/of/(your-subdomain-list)
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	vision "cloud.google.com/go/vision/apiv1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// Sets the name of the image file to annotate.
+	filename := "../testdata/cat.jpg"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	defer file.Close()
+	image, err := vision.NewImageFromReader(file)
+	if err != nil {
+		log.Fatalf("Failed to create image: %v", err)
+	}
+
+	labels, err := client.DetectLabels(ctx, image, nil, 10)
+	if err != nil {
+		log.Fatalf("Failed to detect labels: %v", err)
+	}
+
+	fmt.Println("Labels:")
+	for _, label := range labels {
+		fmt.Println(label.Description)
+	}
+}
+
+// [END vision_quickstart]
